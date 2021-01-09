fbac3aa2d8b8a4fbae61efdfe6b2e2febc95d369
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2021-01-09 21:32:19 +0000

    Port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
-
+const port = process.env.PORT || 3000;
 const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
@@ -12,4 +12,4 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 // Serve files on port 3000
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
