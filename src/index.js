af3af17505878728d5d3e9ee706271ea127855ec
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2021-01-09 15:29:06 +0000

    Printing the XML & CSV data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+import Icon from './images/icon.png';
+import Data from './data/data.xml';
+import Notes from './data/data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -15,6 +17,9 @@ function component() {
     const wolfIcon = new Image();
     wolfIcon.src = Icon;
     element.appendChild(wolfIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
