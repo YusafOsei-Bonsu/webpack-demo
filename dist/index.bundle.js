060006a4190c5f9b7b24979b5a63a39298f97265
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2021-01-09 17:34:22 +0000

    Webpack Rebuild

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -37,7 +37,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var loda
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => /* binding */ printMe\n/* harmony export */ });\nfunction printMe() {\r\n    console.log(\"i get called from print.js\");\r\n}\n\n//# sourceURL=webpack://webpack-demo/./src/print.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => /* binding */ printMe\n/* harmony export */ });\nfunction printMe() {\r\n    console.log(\"I get called from print.js\");\r\n}\n\n//# sourceURL=webpack://webpack-demo/./src/print.js?");
 
 /***/ })
 
