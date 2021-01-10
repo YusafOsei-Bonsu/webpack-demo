dbac5ea7bca37c642f051f77357f9cfe9987ccb9
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2021-01-10 20:48:20 +0000

    Webpack Rebuilt

---
+++ dist/another.bundle.js
@@ -0,0 +1,21 @@
+(self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || []).push([["another"],{
+
+/***/ "./src/another-module.js":
+/*!*******************************!*\
+  !*** ./src/another-module.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+
+
+console.log(lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Another', 'module', 'loaded!'], ' '));
+
+/***/ })
+
+},
+0,[["./src/another-module.js","vendors-node_modules_lodash_lodash_js","shared"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWRlbW8vLi9zcmMvYW5vdGhlci1tb2R1bGUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQXVCOztBQUV2QixZQUFZLGtEQUFNLHlDIiwiZmlsZSI6ImFub3RoZXIuYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IF8gZnJvbSBcImxvZGFzaFwiO1xyXG5cclxuY29uc29sZS5sb2coXy5qb2luKFsnQW5vdGhlcicsICdtb2R1bGUnLCAnbG9hZGVkISddLCAnICcpKTsiXSwic291cmNlUm9vdCI6IiJ9
