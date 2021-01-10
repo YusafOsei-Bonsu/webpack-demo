681b0c5d5b6e670cb0b466d65cdb8924a6b83c82
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2021-01-10 20:44:49 +0000

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-       index: './src/index.js',
-       print: './src/print.js'
+        index: { 
+           import : './src/index.js',
+           dependOn: 'shared'
+        },
+        another: { 
+           import: './src/another-module.js' ,
+           dependOn: 'shared'
+        },
+        shared: 'lodash'
+       // print: './src/print.js',
     },
     devtool: 'inline-source-map', // locates the file where an error has occurred 
     devServer: {
         contentBase: './dist' // telling webpack-dev-server to serve files in '/dist' directory
     },
     plugins: [
-        new CleanWebpackPlugin({ 
-            cleanStaleWebpackAssets: false,
-            cleanOnceBeforeBuildPatterns: ['!index.html', '!index.bundle.js', '!print.bundle.js']
-        }),
         new HtmlWebpackPlugin({ title: "Learning Webpack" })
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/' // Ensures files are served on http://localhost:3000
+        // publicPath: '/' // Ensures files are served on http://localhost:3000
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
 }
